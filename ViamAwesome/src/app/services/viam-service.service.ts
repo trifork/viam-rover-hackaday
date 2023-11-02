db6310000fce748f277b158b2a62b0671cacf3f8
Simon <sak@trifork.com>
2023-11-02 09:16:21 +0100

    Cleaned up viam service

--- ViamAwesome/src/app/services/viam-service.service.ts
+++ ViamAwesome/src/app/services/viam-service.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
-import {createRobotClient, BoardClient, MotorClient, BaseClient, CameraClient, DataManagerClient, EncoderClient, MovementSensorClient} from '@viamrobotics/sdk';
+import {createRobotClient, BoardClient, MotorClient, BaseClient, CameraClient, DataManagerClient, EncoderClient, MovementSensorClient, RobotClient} from '@viamrobotics/sdk';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViamService {
+  private localBoardClient?: BoardClient;
+  private rightMotorClient?: MotorClient;
+  private leftMotorClient?: MotorClient;
+  private baseClient?: BaseClient;
+  private cameraClient?: CameraClient;
+  private rencEncoderClient?: EncoderClient;
+  private accelerometerMovementClient?: MovementSensorClient;
+  private dataManagementClient?: DataManagerClient;
+  private robotClient?: RobotClient;
 
   constructor() { }
 
@@ -15,7 +24,7 @@ export class ViamService {
       const secret = 'robot-location-secret';
       const key = 'x8ml9izy7z6p5esu1eufr4zs30hv9s60sb12fyu45ppx1j7f';
       const addr = 'https://app.com:443';
-      const robot = await createRobotClient({
+      this.robotClient = await createRobotClient({
         host,
         credential: {
           type: secret,
@@ -28,64 +37,61 @@ export class ViamService {
     
     // Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
     // local
-    const localClient = new BoardClient(robot, 'local');
-    const localReturnValue = await localClient.getGPIO('16');
-    console.log('local getGPIO return value:', localReturnValue);
+    this.localBoardClient = new BoardClient(this.robotClient, 'local');
   
     // right
-    const rightClient = new MotorClient(robot, 'right');
-    const rightReturnValue = await rightClient.isMoving();
-    console.log('right isMoving return value:', rightReturnValue);
-
-    //rightClient.setPower(-0.5)
-    rightClient.stop()
+    this.rightMotorClient = new MotorClient(this.robotClient, 'right');
   
     // left
-    const leftClient = new MotorClient(robot, 'left');
-    const leftReturnValue = await leftClient.isMoving();
-    console.log('left isMoving return value:', leftReturnValue);
+    this.leftMotorClient = new MotorClient(this.robotClient, 'left');
   
     // viam_base
-    const viamBaseClient = new BaseClient(robot, 'viam_base');
-    const viamBaseReturnValue = await viamBaseClient.isMoving();
-    console.log('viam_base isMoving return value:', viamBaseReturnValue);
+    this.baseClient = new BaseClient(this.robotClient, 'viam_base');
   
     // cam
-    const camClient = new CameraClient(robot, 'cam');
-    const camReturnValue = await camClient.getImage();
-    console.log('cam getImage return value:', camReturnValue);
+    this.cameraClient = new CameraClient(this.robotClient, 'cam');
   
     // Renc
-    const rencClient = new EncoderClient(robot, 'Renc');
-    const rencReturnValue = await rencClient.getProperties();
-    console.log('Renc getProperties return value:', rencReturnValue);
+    this.rencEncoderClient = new EncoderClient(this.robotClient, 'Renc');
   
     // Lenc
-    const lencClient = new EncoderClient(robot, 'Lenc');
-    const lencReturnValue = await lencClient.getProperties();
-    console.log('Lenc getProperties return value:', lencReturnValue);
+    this.rencEncoderClient = new EncoderClient(this.robotClient, 'Lenc');
   
     // accelerometer
-    const accelerometerClient = new MovementSensorClient(robot, 'accelerometer');
-    const accelerometerReturnValue = await accelerometerClient.getLinearAcceleration();
-    console.log('accelerometer getLinearAcceleration return value:', accelerometerReturnValue);
+    this.accelerometerMovementClient = new MovementSensorClient(this.robotClient, 'accelerometer');
   
     // my_lidar
-    const myLidarClient = new CameraClient(robot, 'my_lidar');
-    const myLidarReturnValue = await myLidarClient.getImage();
-    console.log('my_lidar getImage return value:', myLidarReturnValue);
+    this.cameraClient = new CameraClient(this.robotClient, 'my_lidar');
   
     // Data-Management-Service
-    const dataManagementServiceClient = new DataManagerClient(robot, 'Data-Management-Service');
-    const dataManagementServiceReturnValue = await dataManagementServiceClient.sync();
-    console.log('Data-Management-Service sync return value:', dataManagementServiceReturnValue);
+    this.dataManagementClient = new DataManagerClient(this.robotClient, 'Data-Management-Service');
   
     console.log('Resources:');
-    console.log(await robot.resourceNames());
 
     } catch (error) {
         console.log("Error connecting", error);
     }
   }
-  
+
+  private async getAvailableResources() {
+    if(!this.robotClient) return;
+    console.log(await this.robotClient.resourceNames());
+  }
+
+  public leftMotorForward() {
+
+  }
+
+  public leftMotorReverse() {
+
+  }
+
+  public rightMotorForward() {
+
+  }
+
+  public rightMotorReverse() {
+
+  }
+
 }
